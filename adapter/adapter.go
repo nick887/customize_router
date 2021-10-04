914b6abbd8a5c3d14e7d63bf7b4a897dd4f3c14f
Ritchie <2975684744@qq.com>
2021-10-04 22:03:51 +0800

    docs:增加注释

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -1,23 +1,26 @@
 package adapter
 
 import (
-	"time"
-	"github.com/gin-gonic/gin"
 	"github.com/Chronostasys/centralog/centralog"
+	"github.com/gin-gonic/gin"
+	"time"
 )
 
 // @author Ritchie
 // @version 2.0, 2021/10/4
 // @since 2.0.0
 
+// RequestHandler is a type which give the router a restriction
 type RequestHandler func(ctx *gin.Context) (status int, json interface{}, err error)
 
+// ResponseError is a unified response of error
 type ResponseError struct {
 	Time      time.Time `json:"time"`
 	Reason    string    `json:"reason"`
 	RequestId string    `json:"request_id"`
 }
 
+// ErrorAdapter is wrapper of handler
 func ErrorAdapter(handler RequestHandler) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		status, json, err := handler(ctx)
@@ -25,6 +28,7 @@ func ErrorAdapter(handler RequestHandler) gin.HandlerFunc {
 	}
 }
 
+// processResult process the error msg
 func processResult(ctx *gin.Context, status int, json interface{}, err error) {
 	if err != nil {
 		centralog.Error("[request error]").Any("error", err).CtxID(ctx).Log()
@@ -34,6 +38,7 @@ func processResult(ctx *gin.Context, status int, json interface{}, err error) {
 	ctx.JSON(status, json)
 }
 
+// generateErrorMsg generate the error msg
 func generateErrorMsg(ctx *gin.Context, err error) ResponseError {
 	requestId, _ := ctx.Get(centralog.IDKey)
 	return ResponseError{
--- custom/router.go
+++ custom/router.go
@@ -1,18 +1,19 @@
 package custom
 
 import (
-	"github.com/nick887/customize_router/adapter"
 	"github.com/gin-gonic/gin"
+	"github.com/nick887/customize_router/adapter"
 )
 
 // @author Ritchie
 // @version 2.0, 2021/10/4
 // @since 2.0.0
 
+// CustmoRouter is the router we define
 type CustmoRouter struct {
 	gin.IRouter
 }
-
+// GETC is a Encapsulation of GET
 // Tail processing only the last handler
 // processing order exchange
 func (cr CustmoRouter) GETC(relativePath string, tailHandler adapter.RequestHandler, frontHandlers ...gin.HandlerFunc) gin.IRoutes {
@@ -23,10 +24,12 @@ func (cr CustmoRouter) GETC(relativePath string, tailHandler adapter.RequestHand
 		return cr.GET(relativePath, frontHandlers...)
 	}
 }
+// GroupC is a Encapsulation of Group
 func (cr CustmoRouter) GroupC(relativePath string, handlers ...gin.HandlerFunc) *CustmoRouter {
 	return &CustmoRouter{cr.Group(relativePath, handlers...)}
 }
 
+// POSTC is a Encapsulation of POST
 func (cr CustmoRouter) POSTC(relativePath string, tailHandler adapter.RequestHandler, frontHandlers ...gin.HandlerFunc) gin.IRoutes  {
 	if tailHandler == nil {
 		return cr.POST(relativePath, frontHandlers...)
@@ -36,6 +39,7 @@ func (cr CustmoRouter) POSTC(relativePath string, tailHandler adapter.RequestHan
 	}
 }
 
+// DELETEC is a Encapsulation of DELETE
 func (cr CustmoRouter) DELETEC(relativePath string, tailHandler adapter.RequestHandler, frontHandlers ...gin.HandlerFunc) gin.IRoutes  {
 	if tailHandler == nil {
 		return cr.DELETE(relativePath, frontHandlers...)
